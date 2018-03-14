4d31b289cdb5d288646a3f392402f8dfc50b15b1
Josec1996 <josvene74@gmail.com>
2018-03-14 08:14:46 -0600

    Update clientG.c

--- clientG.c
+++ clientG.c
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <netdb.h> 
+#include <time.h> //usleep
 #include <pthread.h>
 
 //directives are above (e.g. #include ...)
@@ -27,37 +28,48 @@ static int room; // how much room left in buffer?
 static char *after; // pointer to position after the received characters
 //main starts below
 
-
-void error(const char *msg){
-    perror(msg);
-    exit(0);
-}
-
 int buffer_message(char * message){
 
-    //read(STDIN_FILENO, after, 256 - inbuf);
-    short flag = -1; // indicates if returned_data has been set 
-    inbuf += 77;
+    int bytes_read = read(STDIN_FILENO, after, 256 - inbuf);
+    short flag = -1; // indicates if returned_data has been set
+    inbuf += bytes_read;
     int where; // location of network newline
-
     // Step 1: call findeol, store result in where
     where = find_network_newline(message, inbuf);
     if (where >= 0) { // OK. we have a full line
-
         // Step 2: place a null terminator at the end of the string
         char * null_c = {'\0'};
         memcpy(message + where, &null_c, 1); 
-
         // Step 3: update inbuf and remove the full line from the clients's buffer
         memmove(message, message + where + 1, inbuf - (where + 1)); 
         inbuf -= (where+1);
         flag = 0;
     }
-
     // Step 4: update room and after, in preparation for the next read
     room = sizeof(message) - inbuf;
     after = message + inbuf;
+    return flag;
+}
 
+int prueba_error(char * message){
+    int bytes_read=sizeof(message);
+    short flag = -1; // indicates if returned_data has been set
+    inbuf += bytes_read;
+    int where; // location of network newline
+    // Step 1: call findeol, store result in where
+    where = find_network_newline(message, inbuf);
+    if (where >= 0) { // OK. we have a full line
+        // Step 2: place a null terminator at the end of the string
+        char * null_c = {'\0'};
+        memcpy(message + where, &null_c, 1); 
+        // Step 3: update inbuf and remove the full line from the clients's buffer
+        memmove(message, message + where + 1, inbuf - (where + 1)); 
+        inbuf -= (where+1);
+        flag = 0;
+    }
+    // Step 4: update room and after, in preparation for the next read
+    room = sizeof(message) - inbuf;
+    after = message + inbuf;
     return flag;
 }
 
@@ -72,12 +84,13 @@ int find_network_newline(char * message, int bytes_inbuf){
 
 int main(int argc , char *argv[]){
     int sock;
-    struct sockaddr_in server; //hostent *serv;
-    char message[256] , server_reply[256];
-	
-	//signal (SIGCHLD, SIG_IGN);
+    struct sockaddr_in server;
+    //char *message , *server_reply, *buffer;
+    //message = (char *) malloc(256);
+    //server_reply = (char *) malloc(256);
+    char message[256] , server_reply[256], buffer[256];
     //Create socket
-    sock = socket(AF_INET , SOCK_STREAM , 0);
+    sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1){
         printf("Could not create socket");
     }
@@ -93,8 +106,7 @@ int main(int argc , char *argv[]){
         perror("connect failed. Error");
         return 1;
     }
-    
-	//listen(sock,5);
+
    //insert the code below into main, after you've connected to the server
 	puts("Connected\n");    
 
@@ -109,14 +121,17 @@ int main(int argc , char *argv[]){
 	//set the initial position of after
 	after = message;
 	//char linea[256];
-	char buffer[256];//={0};
+	//system("/home/gabino/Documentos/ProyectoLabComu/imgb64.sh");
 	int na;
 	char c;
-	FILE *file;
-	file = fopen("tes", "r");
+   	int cs= 0;
+   	char zero[256]={'\0'};
+    FILE *file;
+  	file = fopen("tes", "r");
 	//unsigned long fsize;
-	char chs;//='0';
-	//chs = getchar();
+	system("rm{output}");
+	FILE *out;
+	out= fopen("output","a+t");
 
 	char line[10] = {0};
 	char per[10]={0};
@@ -125,60 +140,112 @@ int main(int argc , char *argv[]){
       printf("Elige: \n1.Enviar mensaje \n 2.Enviar imagen \n 3.Salir ");
       fgets(line, sizeof(line), stdin);
 
-      if (strlen(line) == 2 && (line[0] == '1'))
-      {
+      if (strlen(line) == 2 && (line[0] == '1')){
          printf("Elige a quien enviar el mensaje:\n");
          while(1){
          printf("Digita: \n1.Usuario A \n 2.Usuario B ");
          fgets(per, sizeof(per), stdin);
 
-         if (strlen(per) == 2 && (per[0] == '1' || per[0] == '2' )){
-         //printf("You did it\n");
-         
-         //continue;
+       if (strlen(per) == 2 && (per[0] == '1' || per[0] == '2' )){
+        //puts("Enter message: ");
+        for(;;){
+    	r_set = all_set;
+    	select(maxfd, &r_set, NULL, NULL, &tv);
+    	
+   		if(FD_ISSET(STDIN_FILENO, &r_set)){
+   		
+   		    if(buffer_message(message) == COMPLETE){
+        	    //Send some data
+        	    if(send(sock, message, strlen(message) + 1, 0) < 0)//NOTE: we have to do strlen(message) + 1 because we MUST include '\0'
+        	    {
+        	        puts("Send failed");
+        	        return 1;
+        	    }
+
+        	    puts("Enter message:");
+        	}
+    	}
+		
+    	if(FD_ISSET(sock, &r_set)){
+        	//Receive a reply from the server
+        	if( recv(sock , server_reply , 77, 0) < 0){
+            	puts("recv failed");
+            	break;
+        	}
+        	cs=0;
+        	while(cs<77){
+                if((server_reply[cs]=='\n')&&(cs<76)){
+                	//server_reply[cs]=EOF;
+                	server_reply[cs]='\0';
+                	//memmove(server_reply, server_reply, cs+2);
+                	printf("\ncs es: %i\n",cs);
+                	fputc(EOF, out);
+                	fclose(out);
+                	system("com=$(base64 -d output > out.png)");
+                	usleep(10000);
+                	break;
+                }
+                fputc(server_reply[cs], out);
+                 
+                cs+=1;
+        	}
+        	printf("\nReply:%s", server_reply);
+        	printf("\nCaracter:%i\n", server_reply[cs]);
+        	//server_reply[0]='\0';
+        	memmove(server_reply, server_reply, 256-cs);
+        	strcpy(server_reply, zero);
+        	//usleep(10000);
+    	}
+      	}
       	}
       else{
          printf("Error, intenta de nuevo...\n\n");
 
          const size_t pos = sizeof(per) - 1; 
          if (strlen(per) == pos && per[pos] != '\n') while (fgetc(stdin) != '\n');
-      }
-     }
+             }
+         }
       }
       else if(strlen(line) == 2 && (line[0] == '2')){
       printf("Elige a quien enviar la imagen:\n");
-        while(1){
+        //while(1){
         printf("Digita: \n1.Usuario A \n 2.Usuario B ");
         fgets(per, sizeof(per), stdin);
+        
       	if(strlen(per) == 2 && (per[0] == '1' || per[0] == '2')){
-			
-		for(;;){
     	r_set = all_set;
+    
    		//check to see if we can read from STDIN or sock
-    		//select(maxfd, &r_set, NULL, NULL, &tv);
-	
-		while ((c= getc(file)) != EOF){
+        //select(maxfd, &r_set, NULL, NULL, &tv);
+		while ((c=getc(file)) != EOF){
         	na=0;
-			while (c != '\n'){
-				buffer[na]=c;//char *strncpy(char *buffer[na], char *c,size_t 1);
-				//printf("este es el contenido de mensaje %c\n", buffer[na]);
+			while (c!='\n')//|| (c!=EOF))
+			{
+				buffer[na]=c;//almacene datos en el arreglo
+				//printf("este es el contenido de mensaje %c\n", c);
 				na+=1;
-				//usleep(500000);
+				//usleep(100000);
 				c= getc(file);
+				if(c==EOF){
+					printf("Hay un EOF");
+				    break;
+				}
+				
 			}
-				buffer[na]=c;
-				printf("este es el contenido de mensaje %s\t", buffer);
-				//Send some data
-				send(sock, buffer, strlen(buffer), 0);
-	        	   //if(send(sock, buffer, strlen(buffer), 0) < 0)//sock, message, strlen(message) + 1, 0) < 0) ::NOTE: we have to do strlen(message) + 1 because we MUST include '\0'
-	        	    //{
-	        	    //    puts("Send failed");
-	        	    //    return 1;
-	        	    //}
-        	}
+			    //printf("c tiene %i\n", c);
+			    buffer[na]=c;
+			    //na+=1;
+			    //buffer[0]= na +'0'; //longitud de linea
 
+				//if(prueba_error(buffer)==COMPLETE){
+	        	if(send(sock, buffer, 77, 0) < 0)// ::NOTE: we have to do strlen(message) + 1 because we MUST include '\0'
+	        	    {
+	        	        puts("Send failed");
+	        	        return 1;
+	        	    }
+        	//}
+        	}
 		fclose(file);
-    	}
     	}      
       	else{
          printf("Error, intenta de nuevo...\n\n");
@@ -187,7 +254,7 @@ int main(int argc , char *argv[]){
          if (strlen(per) == pos && per[pos] != '\n') while (fgetc(stdin) != '\n');
       	}
       	
-      	}	
+      	//}	
       }
       else
       {
@@ -198,82 +265,6 @@ int main(int argc , char *argv[]){
       }
    }
 
-	/*if (file == NULL) {
-    	printf("File not found!\n");
-    	return 1;
-	}
-	else {
-    	fseek(file, 0, SEEK_END);
-    	fsize = ftell(file);
-    	rewind(file);
-
-    	printf("File contains %ld bytes!\n", fsize);
-    	//printf("Sending the file now");
-	}*/
-	//keep communicating with server
-
-	
-	
-	for(;;){
-		//void *p = buffer;
-    	r_set = all_set;
-   		//check to see if we can read from STDIN or sock
-    	select(maxfd, &r_set, NULL, NULL, &tv);
-	
-	//int bytes_read = read(c=getc(file), buffer,fsize);
-	
-
-    	/*if (bytes_read == 0) // We're done reading from the file
-        	break;
-
-	    if (bytes_read < 0){
-	        error("ERROR reading from file, too bad"); 
-	    }*/
-    	
-   		//if(FD_ISSET(STDIN_FILENO, &r_set)){
-		while ((c= getc(file)) != EOF){
-
-        		//if(buffer_message(c) == COMPLETE){
-        	na=0;
-			while (c != '\n'){
-				buffer[na]=c;
-				printf("este es el contenido de mensaje %c\n", buffer[na]);
-				na+=1;
-				//usleep(500000);
-				c= getc(file);
-			}
-				buffer[na]=c;
-				//Send some data
-	        	   if(send(sock, buffer, strlen(buffer)+1, 0) < 0)//sock, message, strlen(message) + 1, 0) < 0) ::NOTE: we have to do strlen(message) + 1 because we MUST include '\0'
-	        	    {
-	        	        puts("Send failed");
-	        	        return 1;
-	        	    }	
-	        	    //puts("Enter message:");
-        	}
-        	/*while (bytes_read > 0){
-        		int bytes_written = write(sock, buffer, bytes_read);
-		        if (bytes_written <= 0){
-		            error("ERROR writing to socket\n");
-        		}
-        		bytes_read -= bytes_written;
-        		p += bytes_written;
-    		}*/
-		fclose(file);
-    	}
-		
-    	if(FD_ISSET(sock, &r_set)){
-        	//Receive a reply from the server
-        	if( recv(sock , server_reply , 78 , 0) < 0){
-        		
-            	puts("recv failed");
-            	//break;
-        	}
-
-        	printf("\nServer Reply: %s\n", server_reply);
-        	server_reply[0]='\0';
-
-    	}
     	close(sock);
 		return 0;
 		//end of main
